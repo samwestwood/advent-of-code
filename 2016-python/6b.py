16c65ef517ec98b59d5c80d1be0a880bc5fa6c3b
Sam Westwood <spwestwood@gmail.com>
2016-12-06 20:58:18 +0000

    Day 6 solutions

---
+++ 2016-python/6b.py
@@ -0,0 +1,23 @@
+#! /usr/local/bin/python3
+
+"""
+Advent of Code 2016 solution - Day 6, part 2
+"""
+
+import os.path
+import pandas as pd
+
+DIR_NAME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+FILE = open(DIR_NAME + "/2016-inputs/6.txt")
+
+
+def solve(data):
+    """Returns least common letter for each column"""
+
+    df = pd.read_csv(data, header=None)[0].apply(lambda x: pd.Series([i for i in list(x)]))
+    result = ''
+    for column in df:
+        result += df[column].value_counts().idxmin()
+    return(result)
+
+print(solve(FILE))
